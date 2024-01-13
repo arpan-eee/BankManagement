460b87d4437edcfcbfa62cd59de7cde564592b4c
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-01-13 15:38:48 +0600

    csrf trusted origin

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://mamarbank-qc6r.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
